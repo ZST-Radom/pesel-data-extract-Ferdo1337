e185097c64f0b0bf3196d791917958fd50f14974
Łukasz Wąsik <wasik.lukasz@SZKOLA.ZST-RADOM.EDU.PL>
2024-10-18 15:30:57 +0200

    to nie łukasz

--- SD.pesel/PersonId.cs
+++ SD.pesel/PersonId.cs
@@ -1,64 +1,70 @@
-﻿namespace SD.pesel;
-
-public class PersonId
+﻿namespace SD.pesel
 {
-    private readonly string _id;
+    public class PersonId
+    {
+        private readonly string _id;
 
-    public PersonId(string Id)
-    {
-        _id = Id;
-    }
-    /// <summary>
-    /// Get full year from PESEL
-    /// </summary>
-    /// <returns></returns>
-    public int GetYear()
-    {
-        return 0;
-    }
+        public PersonId(string id)
+        {
+            if (id.Length != 11 || !long.TryParse(id, out _))
+            {
+                throw new ArgumentException("PESEL must be an 11-digit number.");
+            }
+            _id = id;
+        }
 
-    /// <summary>
-    /// Get month from PESEL
-    /// </summary>
-    public int GetMonth()
-    {
-        return 0;
-    }
+        public int GetYear()
+        {
+            int year = int.Parse(_id.Substring(0, 2));
+            int month = int.Parse(_id.Substring(2, 2));
 
-    /// <summary>
-    /// Get day from PESEL
-    /// </summary>
-    /// <returns></returns>
-    public int GetDay()
-    {
-        return 0;
-    }
+            if (month > 80) return year + 1800;
+            if (month > 60) return year + 2200;
+            if (month > 40) return year + 2100;
+            if (month > 20) return year + 2000;
 
-    /// <summary>
-    /// Get year of birth from PESEL
-    /// </summary>
-    /// <returns></returns>
-    public int GetYearOfBirth()
-    {
-        return 0;
-    }
+            return year + 1900;
+        }
 
-    /// <summary>
-    /// Get gender from PESEL
-    /// </summary>
-    /// <returns>m</returns>
-    /// <returns>f</returns>
-    public string GetGender()
-    {
-        return "";
-    }
+        public int GetMonth()
+        {
+            int month = int.Parse(_id.Substring(2, 2));
+            if (month > 80) return month - 80;
+            if (month > 60) return month - 60;
+            if (month > 40) return month - 40;
+            if (month > 20) return month - 20;
 
-    /// <summary>
-    /// check if PESEL is valid
-    /// </summary>
-    /// <returns></returns>
-    public bool IsValid()
-    {
-        return true;
+            return month;
+        }
+
+        public int GetDay()
+        {
+            return int.Parse(_id.Substring(4, 2));
+        }
+
+        public int GetYearOfBirth()
+        {
+            return GetYear();
+        }
+
+        public string GetGender()
+        {
+            int genderDigit = int.Parse(_id[9].ToString());
+            return genderDigit % 2 == 0 ? "k" : "m"; // k for female, m for male
+        }
+
+        public bool IsValid()
+        {
+            int[] weights = { 1, 3, 7, 9, 1, 3, 7, 9, 1, 3 };
+            int sum = 0;
+
+            for (int i = 0; i < 10; i++)
+            {
+                sum += int.Parse(_id[i].ToString()) * weights[i];
+            }
+
+            int checkDigit = (10 - (sum % 10)) % 10;
+            return checkDigit == int.Parse(_id[10].ToString());
+        }
     }
 }
